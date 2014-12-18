5fcaa95e1c9a31038916467cb453af191d68ec84
Jamie Purchase <jamie.purchase@medicapp.com>
2014-12-18 16:11:48 +0000

    Redirect test in place

--- lists/views.py
+++ lists/views.py
@@ -1,9 +1,13 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from lists.models import Item
 
 def home_page(request):
 	if request.method == 'POST':
-		return render(request, 'home.html', {
-			'new_item_text': request.POST['item_text'],
-		})
-	return render(request, 'home.html')
+		Item.objects.create(text=request.POST['item_text'])
+		return redirect('/')
+	else:
+		new_item_text = ''
+	return render(request, 'home.html', {
+		'new_item_text': new_item_text,
+	})
