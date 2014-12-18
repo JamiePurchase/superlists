5d16fea113365686e5062c2a0721d53e554d0064
Jamie Purchase <jamie.purchase@medicapp.com>
2014-12-18 15:25:51 +0000

    Using a method to check rows in the table

--- functional_tests.py
+++ functional_tests.py
@@ -13,6 +13,11 @@ class NewVisitorTest(unittest.TestCase):
 	def tearDown(self):
 		self.browser.quit()
 		
+	def check_for_row_in_list_table(self, row_text):
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = table.find_elements_by_tag_name('tr')
+		self.assertIn(row_text, [row.text for row in rows])
+		
 	# We use the word test at the start so that the test runner knows that it's a test
 	def test_can_start_a_list_and_retrieve_it_later(self):
 		# Jamie is going to try a new online to-do app.
@@ -34,11 +39,7 @@ class NewVisitorTest(unittest.TestCase):
 		# When he hits enter, the page updates and now the page shows;
 		# "1: Buy GTAV for PS4" as an item in a to-do list
 		inputbox.send_keys(Keys.ENTER)
-		import time
-		# time.sleep(10)
-		table = self.browser.find_element_by_id('id_list_table')
-		rows = table.find_elements_by_tag_name('tr')
-		self.assertIn('1: Buy GTAV for PS4', [row.text for row in rows])
+		self.check_for_row_in_list_table('1: Buy GTAV for PS4')
 
 		# There is still a text box inviting him to add another item
 		# He enters "Buy FFVIIHD on SEN" (Jamie's favourite game in the world)
@@ -47,10 +48,8 @@ class NewVisitorTest(unittest.TestCase):
 		inputbox.send_keys(Keys.ENTER)
 
 		# The page updates again and now shows both items on his list
-		table = self.browser.find_element_by_id('id_list_table')
-		rows = table.find_elements_by_tag_name('tr')
-		self.assertIn('1: Buy GTAV for PS4', [row.text for row in rows])
-		self.assertIn('2: Buy FFVIIHD on SEN', [row.text for row in rows])
+		self.check_for_row_in_list_table('1: Buy GTAV for PS4')
+		self.check_for_row_in_list_table('2: Buy FFVIIHD on SEN')
 
 		# Jamie wonders if the site will remember his to-do items
 		# He sees that the site has generated a unique URL for him
